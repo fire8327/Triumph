770513f53e5f38c21111eb5d9767544a41b16e7a
fire8327 <fire83274@gmail.com>
2024-05-03 20:43:28 +0300

    upd feedback

--- components/Feedback.vue
+++ components/Feedback.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center text-center h-fit gap-6 rounded-xl border border-[#b684b3] transition-all duration-500 hover:shadow-none shadow-[0px_0px_13px_-7px_black] p-4">
-        <p class="font-Comfortaa text-2xl">Как заказать?</p>
-        <p class="text-lg max-w-2xl opacity-70">Сделать заказ у нас легко! Просто оставьте заявку ниже, и мы поможем вам выбрать самый подходящий пакет услуг и забронировать дату вашего мероприятия.</p>
+        <p class="font-Comfortaa text-2xl">Остались вопросы?</p>
+        <p class="text-lg max-w-2xl opacity-70">Если у вас возникли вопросы относительно наших услуг, доступных дат или ценовой политики, пожалуйста, свяжитесь с нами. Мы всегда рады обсудить ваши потребности и предложить индивидуальные решения, соответствующие вашим ожиданиям.</p>
         <button @click="isFeedbackShow = true" class="py-0.5 px-4 rounded-full bg-[#b684b3] border border-[#b684b3] text-white transition-all duration-500 hover:text-[#b684b3] hover:bg-transparent max-w-2xl">Оставить заявку</button>
     </div>
     <div @click="isFeedbackShow = false" class="fixed bg-black/70 inset-0 z-[5] transition-all duration-500" :class="{'-translate-x-full' : !isFeedbackShow}"></div>
