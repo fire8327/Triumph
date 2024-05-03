b9d437aa3e7e4f9b53199cd878307dc505bfb003
fire8327 <fire83274@gmail.com>
2024-05-03 17:43:24 +0300

    create team

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
                     <p class="font-Comfortaa">Индивидуальный подход</p>
                     <Icon name="ic:sharp-person-search" class="text-3xl text-[#b684b3]"/>
                 </div>
-                <p class="opacity-70">Мы понимаем, что каждый ребенок уникален, поэтому наши праздники разрабатываются с учетом интересов и предпочтений каждого маленького гостя.</p>
+                <p class="opacity-70">Мы понимаем, что каждый человек уникален, поэтому наши праздники разрабатываются с учетом интересов и предпочтений каждого гостя.</p>
             </div>
             <div class="flex flex-col gap-4 rounded-xl border border-[#b684b3] transition-all duration-500 hover:shadow-none shadow-[0px_0px_13px_-7px_black] p-4">
                 <div class="flex items-center gap-4">
@@ -43,6 +43,32 @@
                     <Icon name="material-symbols:health-and-safety" class="text-3xl text-[#b684b3]"/>
                 </div>
                 <p class="opacity-70">Мы уделяем особое внимание качеству предоставляемых услуг и своевременной организации мероприятий. Вы можете быть уверены, что ваше мероприятие пройдет гладко и без сбоев.</p>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col gap-6 text-lg">
+        <p class="font-Comfortaa text-2xl">Наша команда</p>
+        <p class="opacity-70">Наши аниматоры - это настоящие профессионалы своего дела, полные энтузиазма и творческой энергии. Они обладают богатым опытом работы и знают, как создать атмосферу радости и веселья на любом мероприятии.</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+            <div class="flex flex-col gap-6">
+                <img src="/images/team/1.png" alt="" class="rounded-xl bg-[#D5EDFE]">
+                <p class="text-xl font-Comfortaa">Дмитрий Петров</p>
+                <p class="opacity-70">Планирует, координирует и реализует мероприятия от начала до конца, включая выбор места проведения, составление бюджета, работу с поставщиками, а также обеспечение безупречного хода события.</p>
+            </div>
+            <div class="flex flex-col gap-6">
+                <img src="/images/team/2.png" alt="" class="rounded-xl bg-[#FBDEED]">
+                <p class="text-xl font-Comfortaa">Ирина Кузнецова</p>
+                <p class="opacity-70">Создаёт атмосферу мероприятия через декорации, цветовое оформление, освещение и другие элементы, чтобы подчеркнуть тему и общий стиль события.</p>
+            </div>
+            <div class="flex flex-col gap-6">
+                <img src="/images/team/3.png" alt="" class="rounded-xl bg-[#F2F8DA]">
+                <p class="text-xl font-Comfortaa">Ольга Николаева</p>
+                <p class="opacity-70">Захватывает ключевые моменты события через фотографии или видео, создавая незабываемые визуальные воспоминания для участников.</p>
+            </div>
+            <div class="flex flex-col gap-6">
+                <img src="/images/team/4.png" alt="" class="rounded-xl bg-[#FBE4D4]">
+                <p class="text-xl font-Comfortaa">Максим Федоров</p>
+                <p class="opacity-70">Отвечает за планирование и управление логистикой мероприятия, включая транспорт, размещение гостей, распределение ресурсов и управление временем.</p>
             </div>
         </div>
     </div>
