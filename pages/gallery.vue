9e2815309047249e6d98747c8d4861cbbbaac9be
fire8327 <fire83274@gmail.com>
2024-05-05 03:02:34 +0300

    upd

---
+++ pages/gallery.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-col gap-6">
+        <p class="font-Comfortaa text-3xl">Галерея</p>
+        <p class="text-xl opacity-70">Загляните в наши восхитительные моменты! Добро пожаловать в нашу фотогалерею, где каждый кадр - история волнующих событий. От захватывающих моментов наших корпоративных мероприятий до волнующих деталей свадебных церемоний, здесь вы найдете вдохновение и эмоциональное воспоминание.</p>
+        <p class="text-xl opacity-70">Наши фотографии отражают не только красоту момента, но и нашу страсть к созданию незабываемых мероприятий. Погрузитесь в мир ярких красок, эмоций и удивительных деталей, которые делают каждое событие особенным.</p>
+        <p class="text-xl opacity-70">Проведите время, наслаждаясь нашей коллекцией фотографий, и дайте вдохновение стать частью следующего незабываемого момента в вашей жизни. События становятся незабываемыми, когда они остаются в памяти, и наши фотографии помогут вам сохранить эти моменты на всю жизнь.</p>
+        <Swiper :loop="true" :space-between="20" :modules="[SwiperAutoplay, SwiperPagination, SwiperNavigation]" :navigation="{enabled: true}" :pagination="{clickable: true}" :autoplay="{delay:3500}" class="w-full">
+            <SwiperSlide class="w-full">
+                <img class="object-cover aspect-video w-full rounded-xl" src="/images/hero/1.jpg" alt="">            
+            </SwiperSlide>
+            <SwiperSlide class="w-full">
+                <img class="object-cover aspect-video w-full rounded-xl" src="/images/hero/2.jpg" alt="">            
+            </SwiperSlide>
+            <SwiperSlide class="w-full">
+                <img class="object-cover aspect-video w-full rounded-xl" src="/images/hero/3.jpg" alt="">            
+            </SwiperSlide>
+            <SwiperSlide class="w-full">
+                <img class="object-cover aspect-video w-full rounded-xl" src="/images/hero/4.jpg" alt="">            
+            </SwiperSlide>
+            <SwiperSlide class="w-full">
+                <img class="object-cover aspect-video w-full rounded-xl" src="/images/hero/5.jpg" alt="">            
+            </SwiperSlide>
+        </Swiper>
+    </div>
+    <ServicesBrief></ServicesBrief> 
+</template>
+
+<script setup>
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -46,33 +46,7 @@
             </div>
         </div>
     </div>
-    <div class="flex flex-col gap-6 text-xl">
-        <p class="font-Comfortaa text-3xl">Наши услуги</p>
-        <p class="opacity-70">Мы предлагаем широкий спектр услуг для успешной реализации вашего мероприятия. Независимо от его масштаба и характера, наша команда готова обеспечить вам все необходимое для создания незабываемого и эффективного события.</p>
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-            <div class="flex flex-col gap-6 items-center">
-                <img src="/images/services/1.jpg" alt="" class="rounded-xl aspect-[7/10] object-cover">
-                <p class="text-xl font-Comfortaa">Детские праздники</p>   
-                <p class="opacity-70 text-justify">Создание волшебной атмосферы с помощью оформления, соответствующего выбранной теме, будь то сказочные герои, мультфильмы или приключения.</p>             
-            </div>
-            <div class="flex flex-col gap-6 items-center">
-                <img src="/images/services/2.jpg" alt="" class="rounded-xl aspect-[7/10] object-cover">
-                <p class="text-xl font-Comfortaa">Праздник под ключ</p> 
-                <p class="opacity-70 text-justify">Организация мероприятия от начала до конца, включая выбор места проведения, разработку темы, составление бюджета и работу с поставщиками.</p>                
-            </div>
-            <div class="flex flex-col gap-6 items-center">
-                <img src="/images/services/3.jpg" alt="" class="rounded-xl aspect-[7/10] object-cover">
-                <p class="text-xl font-Comfortaa">Фотосессии</p> 
-                <p class="opacity-70 text-justify">Использование высококачественной фототехники и освещения для создания красивых и запоминающихся изображений.</p>                
-            </div>
-            <div class="flex flex-col gap-6 items-center">
-                <img src="/images/services/4.jpg" alt="" class="rounded-xl aspect-[7/10] object-cover">
-                <p class="text-xl font-Comfortaa">Аренда</p> 
-                <p class="opacity-70 text-justify">Предоставление широкого выбора мебели, декораций, звукового и светового оборудования для вашего мероприятия.</p>                
-            </div>
-        </div>
-        <NuxtLink to="/" class="py-0.5 px-4 rounded-full bg-[#b684b3] border border-[#b684b3] text-white transition-all duration-500 hover:text-[#b684b3] hover:bg-transparent w-fit self-end">Узнать подробнее</NuxtLink>
-    </div>
+    <ServicesBrief></ServicesBrief>    
     <Feedback></Feedback>
 </template>
 
