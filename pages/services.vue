0a415727ff4510a52ba4b982aaac39b92e08a48d
fire8327 <fire83274@gmail.com>
2024-05-07 02:24:01 +0300

    upd services form

--- pages/services.vue
+++ pages/services.vue
@@ -82,6 +82,7 @@
                 <FormKit v-model="servicesForm.phone" type="text" name="Телефон" validation="required|length:11" messages-class="text-[#E9556D] font-Comfortaa text-base mt-2" input-class="px-4 py-1 border border-[#b684b3] focus:outline-none rounded-[20px] w-full" placeholder="Телефон"/>
                 <FormKit v-model="servicesForm.service" type="select" name="Выбранная услуга" validation="required" :options="['Детские праздники','Корпоратив','Свадьба','Праздник под ключ']" messages-class="text-[#E9556D] font-Comfortaa text-base mt-2" input-class="px-4 py-1 border border-[#b684b3] focus:outline-none rounded-[20px] w-full" placeholder="Выбранная услуга"/>
                 <FormKit v-model="servicesForm.serviceDesc" type="textarea" name="Описание мероприятия" messages-class="text-[#E9556D] font-Comfortaa text-base mt-2" input-class="px-4 py-1 border border-[#b684b3] focus:outline-none rounded-[20px] w-full" placeholder="Кратко опишите своё мероприятие"/>
+                <FormKit v-model="servicesForm.additional" type="checkbox" name="Дополнительные услуги" :options="['Фотограф', 'Аниматор', 'Кейтеринг']" messages-class="text-[#E9556D] font-Comfortaa text-base mt-2" input-class="px-4 py-1 border border-[#b684b3] focus:outline-none rounded-[20px] mr-5" legend-class="opacity-80 font-Comfortaa" label="Дополнительные услуги"/>
             </div>
             <button class="w-fit text-center mx-auto py-0.5 px-4 rounded-full bg-[#b684b3] border border-[#b684b3] text-white transition-all duration-500 hover:text-[#b684b3] hover:bg-transparent">Оформить</button>
         </FormKit>    
@@ -112,5 +113,6 @@
     /* выбор услуги */
     const selectService = (service, price) => {
         servicesForm.value.service = service
+        servicesForm.value.price = price
     }
 </script>
